fc6e6bae7d7be9e3cf648bb4e9a7d68a4b43163e
Anthony NACHER <nacheranthony@gmail.com>
2023-04-04 23:20:59 +0200

    Added TEST : filter by room + hardcoded values for date created and UUID

--- api/tests.py
+++ api/tests.py
@@ -14,31 +14,41 @@ class APITestCase (TestCase):
         living_room.save()
 
         entity = Entity(
-            id=get_uuid_as_hex(),
+            id="00000000-0000-0000-0000-000000000001",
             name="Ceiling Light",
             type=Entity.READABLE_TYPES["light"],
             status=Entity.READABLE_STATUS["off"],
             value=None,
-            room_id=kitchen.id)
+            room_id=kitchen.id,
+            created_at='')
+        entity.save()
+        entity.created_at="2023-04-04T21:17:56"
         entity.save()
 
         entity = Entity(
-            id=get_uuid_as_hex(),
+            id="00000000-0000-0000-0000-000000000002",
             name="Lamp",
             type=Entity.READABLE_TYPES["light"],
             status=Entity.READABLE_STATUS["on"],
             value=200,
-            room_id=living_room.id)
+            room_id=living_room.id,
+            created_at='')
         entity.save()
-
+        entity.created_at="2023-04-04T21:17:56"
+        entity.save()
         entity = Entity(
-            id=get_uuid_as_hex(),
+            id="00000000-0000-0000-0000-000000000003",
             name="Thermometer",
             type=Entity.READABLE_TYPES["sensor"],
             status=Entity.READABLE_STATUS["on"],
             value=28,
-            room_id=living_room.id)
-        entity.save()  
+            room_id=living_room.id,
+            created_at='')
+        
+        entity.save()
+        entity.created_at="2023-04-04T21:17:56"
+        entity.save()
+        
     def test_get_entities(self):
         response = self.client.get("/entities")
         self.assertEqual(response.status_code, 200)
@@ -49,7 +59,7 @@ class APITestCase (TestCase):
                     "type": "light",
                     "status": "off",
                     "value": None,
-                    "created_at": ""
+                    "created_at": "2023-04-04T21:17:56"
                 },
                 {
                     "id": "00000000-0000-0000-0000-000000000002",
@@ -57,7 +67,7 @@ class APITestCase (TestCase):
                     "type": "light",
                     "status": "on",
                     "value": "200",
-                    "created_at": ""
+                    "created_at": "2023-04-04T21:17:56"
                 },
                 {
                     "id": "00000000-0000-0000-0000-000000000003",
@@ -65,7 +75,7 @@ class APITestCase (TestCase):
                     "type": "sensor",
                     "status": "on",
                     "value": "28",
-                    "created_at": ""
+                    "created_at": "2023-04-04T21:17:56"
                 }])
     def test_get_entities_with_type_filter(self):
         response = self.client.get("/entities?type=sensor")
@@ -78,7 +88,7 @@ class APITestCase (TestCase):
                                 "type": "sensor",
                                 "status": "on",
                                 "value": "28",
-                                "created_at": ""
+                                "created_at": "2023-04-04T21:17:56"
                             }
                         ]
                          
@@ -95,7 +105,7 @@ class APITestCase (TestCase):
                                 "type": "light",
                                 "status": "on",
                                 "value": "200",
-                                "created_at": ""
+                                "created_at": "2023-04-04T21:17:56"
                             },
                             {
                                 "id": "00000000-0000-0000-0000-000000000003",
@@ -103,6 +113,29 @@ class APITestCase (TestCase):
                                 "type": "sensor",
                                 "status": "on",
                                 "value": "28",
-                                "created_at": ""
+                                "created_at": "2023-04-04T21:17:56"
+                            }
+                        ])
+        
+    def test_get_entities_with_room_filter(self):
+        response = self.client.get("/entities?room=Living Room")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), 
+                                            [
+                            {
+                                "id": "00000000-0000-0000-0000-000000000002",
+                                "name": "Lamp",
+                                "type": "light",
+                                "status": "on",
+                                "value": "200",
+                                "created_at": "2023-04-04T21:17:56"
+                            },
+                            {
+                                "id": "00000000-0000-0000-0000-000000000003",
+                                "name": "Thermometer",
+                                "type": "sensor",
+                                "status": "on",
+                                "value": "28",
+                                "created_at": "2023-04-04T21:17:56"
                             }
                         ])
