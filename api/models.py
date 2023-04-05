7fa5ac3761ca3ef807fc72495f41a1bb0ca71a50
Anthony NACHER <nacheranthony@gmail.com>
2023-04-06 00:09:43 +0200

    Updated uuidfield as not editable

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 # Create your models here.
+
 def get_uuid_as_hex():
     return uuid.uuid4().hex
 
@@ -12,32 +13,32 @@ class Entity (models.Model):
         "multimedia" : "4",
         "air_conditioner" : "5",
         }
-    TYPES = (
+    TYPES = [
         ('1', 'sensor'),
         ('2', 'light'),
         ('3', 'switch'),
         ('4', 'multimedia'),
         ('5', 'air_conditioner'),
-    )
+    ]
     
     READABLE_STATUS = {
         "on" : "1",
         "off" : "2",
         "unavailable" : "3",
         }
-    STATUS = (
+    STATUS = [
         ('1', 'on'),
         ('2', 'off'),
         ('3', 'unavailable'),
-    )
+    ]
 
-    id = models.UUIDField(primary_key=True, default=get_uuid_as_hex)
+    id = models.UUIDField(primary_key=True, default=get_uuid_as_hex, editable=False)
     name = models.CharField(max_length=600, null=False)
     type = models.CharField(max_length=1, choices=TYPES, null=False)
     status = models.CharField(max_length=1, choices=STATUS, null=False)
     value =  models.CharField(max_length=600, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE, null=True)
     def get_status(self):
         return self.get_status_display()
     def get_type(self):
